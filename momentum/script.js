d4c8389f5c9b31ac23489f428c099a76b7467acb
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2020-10-24 23:43:43 +0400

    fix: fix img's load again

--- momentum/script.js
+++ momentum/script.js
@@ -52,6 +52,28 @@ window.onload = () => {
   viewBgImage();
 }
 
+function preloadImages(array) {
+  if (!preloadImages.list) {
+      preloadImages.list = [];
+  }
+  var list = preloadImages.list;
+  for (var i = 0; i < array.length; i++) {
+      var img = new Image();
+      img.onload = function() {
+          var index = list.indexOf(this);
+          if (index !== -1) {
+              // remove image from the array once it loaded
+              // for memory consumption reasons
+              list.splice(index, 1);
+          }
+      }
+      list.push(img);
+      img.src = array[i];
+  }
+}
+
+preloadImages(wallpapersArr);
+
 function viewBgImage() {
   let src = wallpapersArr[currentImg];
   const img = document.createElement("img");	
